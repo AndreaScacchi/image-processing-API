7b1ef48afb5b9e8701913fed0a1d0dad47710745
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 12:46:45 +0100

    endpoint changed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const port = 5000;
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('NASA explore the Universe🌎🪐☄️🧑‍🚀🔭');
 });
 
 // start the express server
