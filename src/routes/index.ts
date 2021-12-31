0a2998cb665e5e153969d8c8d6acfd74f6df8376
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 17:29:35 +0100

    endpoint created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,8 @@ import express from 'express';
 
 // router object
 const routes = express.Router();
+
+// define endpoint
+app.get('/', (req, res) => {
+    res.send('NASA explore the Universe🌎🪐☄️🧑‍🚀🔭');
+});
