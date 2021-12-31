417eecdd9dfae0983cbee59b313e81b469e5e28d
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 12:19:35 +0100

    endpoint created

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// define a route handler for the default home page
+app.get('/api', (req, res) => {
+    res.send('Server working');
+});
+
 // start the express server
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
