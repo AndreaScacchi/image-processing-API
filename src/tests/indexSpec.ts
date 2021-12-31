f64ea57b7dd5a68cde860472092aba2b25c10005
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 12:54:03 +0100

    test modified

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,8 +3,9 @@ import app from "../index";
 
 const request = supertest(app);
 describe("Test endpoint responses", () => {
-	it("gets the api endpoint", async () => {
+	it("gets the api endpoint", async (done) => {
 		const response = await request.get("/");
 		expect(response.status).toBe(200);
+		done();
 	});
 });
