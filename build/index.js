7d0c279d2a1c2fb511d5541b45f03124b25b5742
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 17:07:50 +0100

    typescript compiled

--- build/index.js
+++ build/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
 var port = 5000;
+app.use(express_1.default.static(path_1.default.join(__dirname, '/images')));
 // define a route handler for the default home page
 app.get('/', function (req, res) {
     res.send('NASA explore the Universe🌎🪐☄️🧑‍🚀🔭');
