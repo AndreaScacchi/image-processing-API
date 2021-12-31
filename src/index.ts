23ce6e460d8d898ff082a48f867baaa2d4cd54e9
AndreaScacchi <andreascacchi10@gmail.com>
2021-12-31 12:10:45 +0100

    port created

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+
+// start the express server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
