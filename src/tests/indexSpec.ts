55019243eceeab59fcde06c8699c8584e6ddb75a
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-01 11:56:50 +0100

    path modified

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,7 +4,7 @@ import app from "../index";
 const request = supertest(app);
 describe("Test endpoint responses", () => {
 	it("gets the api endpoint", async () => {
-		const response = await request.get("/routes");
+		const response = await request.get("/routes/index");
 		expect(response.status).toBe(200);
 	});
 });
