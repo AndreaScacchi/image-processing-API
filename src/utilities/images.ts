3c5c57a7cb86755eef48cf7f52edce82aff997ee
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 14:40:50 +0100

    getImage function created

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -1,3 +1,7 @@
 import { promises as fspromises } from 'fs';
 import path from 'path';
 import sharp from 'sharp';
+
+const getImage = async (imageName: string, width: number, height: number): Promise<string> => {
+    
+}
