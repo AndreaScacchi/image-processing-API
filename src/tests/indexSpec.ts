c692aa1957d8855c178a2ab2cf8eddcfef07da78
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 14:20:13 +0100

    test modified

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,8 +3,16 @@ import app from "../index";
 
 const request = supertest(app);
 describe("Test endpoint responses", () => {
-	it("return the correct response", async () => {
+	it("returns the correct response", async () => {
 		const response = await request.get("src/utilities/images");
 		expect(response.status).toBe(200);
+	});
+	it('returns a 404 error', async () => {
+		const response = await request.get("/bad");
+		expect(response.status).toBe(404);
+	});
+	it("returns a 400 error", async () => {
+		const response = await request.get("/bad");
+		expect(response.status).toBe(400);
 	});
 });
