4b5f455db0a92fb3454965a07d8e8a73874d08bf
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 16:13:02 +0100

    another try catch block added

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -3,11 +3,18 @@ import path from 'path';
 import sharp from 'sharp';
 
 const getImage = async (imageName: string, width: number, height: number): Promise<string> => {
-    const imagesDirectory = path.join(__dirname, 'images');
+    const imagesDirectory = path.join(__dirname, '../images/thumbs/');
     const filePath = path.join(imagesDirectory, `${imageName.toLowerCase()}(${width}x${height}).jpg`);
-    try {
 
-    } catch(e) {
+    try {
+        await fspromises.readdir(imagesDirectory);
+    } catch {
+        await fspromises.mkdir(imagesDirectory);
+    }
+
+    try {
         
+    } catch {
+
     }
 }
