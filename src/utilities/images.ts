032d20cd7fdf8868c6ee4e5a3c1111fa2e605a06
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 16:03:08 +0100

    try catch block declared

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -5,4 +5,9 @@ import sharp from 'sharp';
 const getImage = async (imageName: string, width: number, height: number): Promise<string> => {
     const imagesDirectory = path.join(__dirname, 'images');
     const filePath = path.join(imagesDirectory, `${imageName.toLowerCase()}(${width}x${height}).jpg`);
+    try {
+
+    } catch(e) {
+        
+    }
 }
