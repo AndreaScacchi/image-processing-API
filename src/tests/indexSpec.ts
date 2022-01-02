5317bf83973e524f973bd63ccebe2d43f5244ab4
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 14:28:34 +0100

    test changed

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -7,12 +7,16 @@ describe("Test endpoint responses", () => {
 		const response = await request.get("/");
 		expect(response.status).toBe(200);
 	});
-	it('returns a 404 error', async () => {
-		const response = await request.get("/bad");
+	it('returns a correct response', async () => {
+		const response = await request.get("/images?image=fjord&width=250%height=250");
+		expect(response.status).toBe(200);
+	});
+	it("returns a 404 error", async () => {
+		const response = await request.get("/images?image=wrongName&width=250%height=250");
 		expect(response.status).toBe(404);
 	});
 	it("returns a 400 error", async () => {
-		const response = await request.get("/bad");
+		const response = await request.get("/images?image=wrongName&height=250");
 		expect(response.status).toBe(400);
 	});
 });
