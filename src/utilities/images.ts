dd25d21d300ad60e7960cd4564736c45e1e59646
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-02 16:22:54 +0100

    getImage function edited

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -2,7 +2,7 @@ import { promises as fspromises } from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
-const getImage = async (imageName: string, width: number, height: number): Promise<string | null> => {
+const getImage = async (imageName: string, width: number, height: number): Promise<string> => {
     const imagesDirectory = path.join(__dirname, '../images/thumbs/');
     const filePath = path.join(imagesDirectory, `${imageName.toLowerCase()}(${width}x${height}).jpg`);
 
@@ -14,8 +14,9 @@ const getImage = async (imageName: string, width: number, height: number): Promi
 
     try {
         await fspromises.readFile(filePath, { flag: 'r' });
-        return filePath;
-    } catch {
-        return null;
+        //return filePath;
+    } catch(err) {
+        console.log(`error ${err}`);
     }
+    return filePath;
 }
