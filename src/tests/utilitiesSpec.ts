4b7829a9407732b07799d91455152a7a7b6b8008
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-04 17:48:49 +0100

    first test added

--- src/tests/utilitiesSpec.ts
+++ src/tests/utilitiesSpec.ts
@@ -1,2 +1,12 @@
 import path from 'path';
 import { getImage, resizeImage } from '../utilities/processImages';
+
+describe('Test the image', () => {
+    beforeAll(async () => {
+        await resizeImage('fjord', 200, 200);
+    });
+    it('returns the image path', async () => {
+        const filePath = path.join(__dirname, "../../images/thumbs/", 'fjord(200x200).jpeg');
+        expect(await getImage('fjord', 200, 200)).toBe(filePath);
+    });
+});
