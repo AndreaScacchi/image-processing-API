194113909e07b7b83cfd157be6c8a5ab8c8c2791
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-04 14:31:47 +0100

    extension modified from jpg to jpeg

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -23,7 +23,8 @@ const getImage = async (imageName: string, width: number, height: number): Promi
 
 const resizeImage = async (imageName: string, width: number, height: number): Promise<String> => {
     const imagesPath = path.join(__dirname, '/images', `${imageName.toLowerCase()}(${width}x${height}).jpg`);
-    const outputImages = path.join(__dirname, '../images/thumbs', `${imageName.toLowerCase()}(${width}x${height}).jpg`);
+    const outputImages = path.join(__dirname, '../images/thumbs', `${imageName.toLowerCase()}(${width}x${height}).jpeg`);
+
     
     try {
         await fspromises.open(imagesPath, 'r');
