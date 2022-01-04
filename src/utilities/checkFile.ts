d39d55340668c06e822e3dfa35c978a3efc2d59c
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-04 18:36:22 +0100

    try catch block added

--- src/utilities/checkFile.ts
+++ src/utilities/checkFile.ts
@@ -2,7 +2,11 @@ import { access } from 'fs/promises';
 import { constants } from 'fs';
 
 const checkFile = async (Path: string): Promise<boolean> => {
+    try {
 
+    } catch (err) {
+
+    }
 };
 
 export default checkFile;
