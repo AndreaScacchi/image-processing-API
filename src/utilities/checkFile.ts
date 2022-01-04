fa5590f47155184c0c720ca9c558a62ed9fc97d9
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-04 18:38:57 +0100

    function implemented

--- src/utilities/checkFile.ts
+++ src/utilities/checkFile.ts
@@ -3,9 +3,11 @@ import { constants } from 'fs';
 
 const checkFile = async (Path: string): Promise<boolean> => {
     try {
-
+        await access(Path, constants.R_OK | constants.W_OK);
+        return true;
     } catch (err) {
-
+        if (err instanceof Error) console.log(`Error: ${err.message}`);
+        return false;
     }
 };
 
