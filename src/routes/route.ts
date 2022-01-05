d24f06be6ab00afedcc54802b96c33f374f9486f
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-05 17:54:41 +0100

    file modified

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -3,7 +3,11 @@ import { getImage, resizeImage } from '../utilities/processImages';
 const route = express.Router();
 
 route.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
-    
+    const image = await getImage(
+        req.query.resizedImage as string,
+        parseInt(req.query.width as string),
+        parseInt(req.query.height as string)
+    )
 })
 
 
