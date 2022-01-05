260826741de005f10136d2b9dc3da05bf5517036
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-05 17:23:56 +0100

    request and response typed

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import { getImage, resizeImage } from '../utilities/processImages';
 const route = express.Router();
 
+route.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
+    
+})
+
 
 
 
