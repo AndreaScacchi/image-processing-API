b6cccb44dc9851a79a60ccf0c3a8c4b1a5b9eb04
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-06 10:54:39 +0100

    path modified

--- src/utilities/processImages.ts
+++ src/utilities/processImages.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import sharp from 'sharp';
 
 const getImage = async (imageName: string, width: number, height: number): Promise<string> => {
-    const imagesDirectory = path.join(__dirname, '/images');
+    const imagesDirectory = path.join(__dirname, '/images/');
     const filePath = path.join(imagesDirectory, `${imageName.toLowerCase()}(${width}x${height}).jpg`);
 
     try {
