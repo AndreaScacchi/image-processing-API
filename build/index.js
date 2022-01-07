080584cb82da2f4b294435f51f886ba9f98cb01d
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-07 17:41:55 +0100

    typescript compiled

--- build/index.js
+++ build/index.js
@@ -8,7 +8,9 @@ var index_1 = __importDefault(require("./routes/index"));
 var app = (0, express_1.default)();
 var port = 5000;
 // use the router object as middleware
-app.use('/', index_1.default);
+app.use('/', index_1.default, function (req, res) {
+    res.send("helllo");
+});
 // start the express server
 app.listen(port, function () {
     console.log("Server started at http://localhost:".concat(port));
