18f6bb5abecf352716656685d752a2decced914c
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-07 12:13:58 +0100

    body test added

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -14,6 +14,7 @@ describe("Test if iamges are resized", () => {
 	it("returns a valid response", async () => {
 		const response = await request.get("/route?imageName=santamonica&width=500%height=500");
 		expect(response.status).toBe(200);
+		expect(response.body.message).toBe("valid response");
 	});
 	it("returns a 404 error for invalid name image", async () => {
 		const response = await request.get("/route?imageName=wrongName&width=500%height=500");
