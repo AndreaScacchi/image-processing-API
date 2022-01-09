dc7f9ed1824e8320b48b3a852a40ca27916f51a7
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 12:40:03 +0100

    getImage function import commented

--- src/routes/api/route.ts
+++ src/routes/api/route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getImage, resizeImage } from "../../utilities/processImages";
+import { /*getImage,*/ resizeImage } from "../../utilities/processImages";
 const route = express.Router();
 
 route.get(
