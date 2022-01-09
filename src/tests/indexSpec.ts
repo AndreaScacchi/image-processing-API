d786ebc1e69aee8d8814c4a578c4221eed35f729
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 21:51:35 +0100

    refactoring the test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -38,27 +38,3 @@ describe("resize image function tests", () => {
 		expect(response.status).toBe(404);
 	})
 });
-
-/*describe("Test if iamges are resized", () => {
-	it("returns a valid response", async () => {
-		const response = await request.get(
-			"/route?imageName=santamonica&width=500&height=500"
-		);
-		expect(response.status).toBe(200);
-		expect(response.body.message).toBe("valid response");
-	});
-	it("returns a 404 error for invalid name image", async () => {
-		const response = await request.get(
-			"/route?imageName=wrongName&width=500&height=500"
-		);
-		expect(response.status).toBe(404);
-		expect(response.body.message).toBe("invalid name image");
-	});
-	it("returns a 400 error for incomplete request", async () => {
-		const response = await request.get(
-			"/route?imageName=santamonica&height=500"
-		);
-		expect(response.status).toBe(404);
-		expect(response.body.message).toBe("incomplete request");
-	});
-});*/
