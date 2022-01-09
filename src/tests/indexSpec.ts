fc519f0e863049f0f312c6f38de42da08f1a897f
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 14:07:02 +0100

    test modified

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -6,7 +6,7 @@ const request = supertest(app);
 describe("Test endpoint responses", () => {
 	it("gets the API endpoint", async () => {
 		const response = await request.get(
-			"/api/image?imageName=santamonica&width=200&height=200"
+			"/api/route?imageName=santamonica&width=200&height=200"
 		);
 		expect(response.status).toBe(200);
 		//expect(response.body.message).toBe("pass!");
