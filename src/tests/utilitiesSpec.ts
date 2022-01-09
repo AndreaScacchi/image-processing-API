95a5bcbb9aded7c65646c116ac635836b0fb3b1e
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 22:52:48 +0100

    path changed

--- src/tests/utilitiesSpec.ts
+++ src/tests/utilitiesSpec.ts
@@ -2,7 +2,7 @@ import path from "path";
 import checkFile from "../utilities/checkFile";
 
 describe("Test to find out if the file/image exist", async () => {
-	const rightPath = path.resolve("images/thumb/santamonica500x500.jpg");
+	const rightPath = path.join(__dirname, "images/thumb/santamonica500x500.jpg");
 	const incorrectPath = "./images/thumb/santamonic200x200.jpg";
 
 	it("expects that file exist", async () => {
