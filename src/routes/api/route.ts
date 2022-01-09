c4b88005286827dbc6f664928590e7e31a156d33
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 22:32:36 +0100

    refactoring the route

--- src/routes/api/route.ts
+++ src/routes/api/route.ts
@@ -4,46 +4,7 @@ import path from "path";
 import fs from "fs";
 const route = express.Router();
 
-/*route.get(
-	"/",
-	async (req: express.Request, res: express.Response): Promise<void> => {
-		const image = await getImage(
-			req.query.imageName as string,
-			parseInt(req.query.width as string),
-			parseInt(req.query.height as string)
-		);
-
-		if (image) {
-			res.status(200).sendFile(image);
-		} else {
-			try {
-				const width = parseInt(req.query.width as string);
-				const height = parseInt(req.query.height as string);
-				if (!width || !height) {
-					res.status(400).send(
-						"An error occurred with width or height"
-					);
-					return;
-				}
-				const filePath = await resizeImage(
-					req.query.imageName as string,
-					width,
-					height
-				);
-				res.status(200).sendFile(filePath);
-			} catch (err) {
-				if (err instanceof Error) {
-					res.status(404).send(
-						"Provide a valid image, with a valid width and height"
-					);
-				} else {
-					res.send("An error occurred when resize the image");
-				}
-			}
-		}
-	}
-);*/
-
+// implemented the route
 route.get("/", async (req: express.Request, res: express.Response): Promise<void> => {
 	let imagename = req.query.imagename as string;
 	let width = parseInt(req.query.width as string);
