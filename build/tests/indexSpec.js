ad433e1e53be92a51eff1e6394b3d165deb432a0
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 22:15:25 +0100

    typescript compiled

--- build/tests/indexSpec.js
+++ build/tests/indexSpec.js
@@ -60,7 +60,7 @@ describe("Test endpoint responses", function () {
 });
 describe("test if the resizeImage function works", function () {
     var filePath = "/Users/Andrea/Dropbox//PC/Desktop/image-processing-API/images/full/santamonica.jpg";
-    var outputImages = "/Users/Andrea/Dropbox/PC/Desktop/image-processing-API/images/thumb/500-500-santamonica.jpg";
+    var outputImages = "/Users/Andrea/Dropbox/PC/Desktop/image-processing-API/images/thumb/santamonica-500x500.jpg";
     it("returns a valid response with the rigth image file", function () { return __awaiter(void 0, void 0, void 0, function () {
         var response;
         return __generator(this, function (_a) {
