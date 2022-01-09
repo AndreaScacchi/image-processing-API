f555b4ec97e8e12d824d01c59c0113f034339e0d
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 12:30:55 +0100

    main API route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,10 @@ import route from "./api/route";
 // router object
 const routes = express.Router();
 
+routes.get('/', (req: express.Request, res: express.Response): void => {
+    res.send("API route");
+});
+
 routes.use("/route", route);
 
 export default routes;
