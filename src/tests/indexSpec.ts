93b418c114478f904cdede262acdaae70f2ed67a
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 21:53:28 +0100

    refactoring the test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -6,7 +6,7 @@ const request = supertest(app);
 describe("Test endpoint responses", () => {
 	it("gets the image endpoint", async () => {
 		const response = await request.get(
-			"/api/route?filename=fjord&width=300&height=300"
+			"/api/route?filename=santamonica&width=300&height=300"
 		);
 		expect(response.status).toBe(200);
 		expect(response.body.message).toEqual(undefined);
@@ -15,9 +15,9 @@ describe("Test endpoint responses", () => {
 
 describe("resize image function tests", () => {
 	const filePath =
-		"/Users/Andrea/Dropbox//PC/Desktop/image-processing-API/images/full/fjord.jpg";
+		"/Users/Andrea/Dropbox//PC/Desktop/image-processing-API/images/full/santamonica.jpg";
 	const outputImages =
-		"/Users/Andrea/Dropbox/PC/Desktop/image-processing-API/images/thumb/300-300-fjord.jpg";
+		"/Users/Andrea/Dropbox/PC/Desktop/image-processing-API/images/thumb/300-300-santamonica.jpg";
 
 	it("returns the rigth image file", async () => {
 		const response = await resizeImage(filePath, outputImages, 300, 300);
@@ -26,14 +26,14 @@ describe("resize image function tests", () => {
 
 	it('returns a 404 error for invalid image name', async () => {
 		const response = await request.get(
-			"/api/route?filename=fjor&width=300&height=300"
+			"/api/route?filename=santamonic&width=300&height=300"
 		);
 		expect(response.status).toBe(404);
 	})
 
 	it('returns a 404 error for incomplete request', async () => {
 		const response = await request.get(
-			"/api/route?filename=fjor&width=300"
+			"/api/route?filename=santamonica&width=300"
 		);
 		expect(response.status).toBe(404);
 	})
