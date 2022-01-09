1e609619326d76d52c101fb859b9cc9833c9e4f0
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 18:31:07 +0100

    typescript compiled

--- build/routes/api/route.js
+++ build/routes/api/route.js
@@ -116,7 +116,7 @@ route.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, fu
             case 5:
                 err_1 = _a.sent();
                 if (err_1 instanceof Error) {
-                    console.log(err_1.message);
+                    res.status(404).send("There is an error: ".concat(err_1.message));
                 }
                 return [3 /*break*/, 6];
             case 6: return [2 /*return*/];
