84bdf309f7fee7fb9d2ecfb2b442a4e00563cecc
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-09 22:33:21 +0100

    typescript compiled

--- build/routes/api/route.js
+++ build/routes/api/route.js
@@ -44,45 +44,7 @@ var processImages_1 = __importDefault(require("../../utilities/processImages"));
 var path_1 = __importDefault(require("path"));
 var fs_1 = __importDefault(require("fs"));
 var route = express_1.default.Router();
-/*route.get(
-    "/",
-    async (req: express.Request, res: express.Response): Promise<void> => {
-        const image = await getImage(
-            req.query.imageName as string,
-            parseInt(req.query.width as string),
-            parseInt(req.query.height as string)
-        );
-
-        if (image) {
-            res.status(200).sendFile(image);
-        } else {
-            try {
-                const width = parseInt(req.query.width as string);
-                const height = parseInt(req.query.height as string);
-                if (!width || !height) {
-                    res.status(400).send(
-                        "An error occurred with width or height"
-                    );
-                    return;
-                }
-                const filePath = await resizeImage(
-                    req.query.imageName as string,
-                    width,
-                    height
-                );
-                res.status(200).sendFile(filePath);
-            } catch (err) {
-                if (err instanceof Error) {
-                    res.status(404).send(
-                        "Provide a valid image, with a valid width and height"
-                    );
-                } else {
-                    res.send("An error occurred when resize the image");
-                }
-            }
-        }
-    }
-);*/
+// implemented the route
 route.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
     var imagename, width, height, filePath, outputImages, err_1;
     return __generator(this, function (_a) {
