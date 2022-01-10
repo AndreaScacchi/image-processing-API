ef3df56c92c904aaa06def44c310bd8f394371e3
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-10 09:18:29 +0100

    typescript compiled

--- build/routes/api/route.js
+++ build/routes/api/route.js
@@ -39,6 +39,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// import express, resizeImage function, path and fs
 var express_1 = __importDefault(require("express"));
 var processImages_1 = __importDefault(require("../../utilities/processImages"));
 var path_1 = __importDefault(require("path"));
