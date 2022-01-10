1aadb62e68859f4d46c440492624a9405c8fb1af
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-10 08:43:33 +0100

    refactoring the resizeImage function

--- src/utilities/processImages.ts
+++ src/utilities/processImages.ts
@@ -1,35 +1,5 @@
-/*import { promises as fspromises } from "fs";*/
 import path from "path";
 import sharp from "sharp";
-
-/*const getImage = async (
-	imageName: string,
-	width: number,
-	height: number
-): Promise<string> => {
-	const imagesDirectory = path.join(__dirname, "../..", "/images/full/");
-	const filePath = path.join(
-		imagesDirectory,
-		`${imageName}${width}x${height}.jpg`
-	);
-
-	try {
-		await fspromises.readdir(imagesDirectory);
-	} catch {
-		await fspromises.mkdir(imagesDirectory);
-	}
-
-	try {
-		await fspromises.readFile(filePath, { flag: "r" });
-	} catch (err) {
-		let errorMessage = "Got an error trying to read the file";
-		if (err instanceof Error) {
-			errorMessage = `${errorMessage}: ${err.message}`;
-		}
-		console.log(errorMessage);
-	}
-	return filePath;
-};*/
 
 // function to resize the images
 const resizeImage = async (
@@ -51,4 +21,4 @@ const resizeImage = async (
 	return outputImages;
 };
 
-export default /*getImage,*/ resizeImage;
+export default resizeImage;
