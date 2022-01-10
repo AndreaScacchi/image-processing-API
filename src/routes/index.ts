8f3b50c10c5efc0242205f5461fc00697387d34f
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-10 09:11:58 +0100

    refactoring

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import route from "./api/route";
 // router object declared
 const routes = express.Router();
 
+// use the router object for the main API route
 routes.get("/", (req: express.Request, res: express.Response): void => {
 	res.send("API route");
 });
