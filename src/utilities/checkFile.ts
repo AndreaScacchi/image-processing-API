5125a61a3e2996fc5b568bc9fb2e55d66405330b
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-10 08:30:14 +0100

    parameter name modified

--- src/utilities/checkFile.ts
+++ src/utilities/checkFile.ts
@@ -1,9 +1,9 @@
 import { access } from "fs/promises";
 import { constants } from "fs";
 
-const checkFile = async (path: string): Promise<boolean> => {
+const checkFile = async (imagePath: string): Promise<boolean> => {
 	try {
-		await access(path, constants.R_OK | constants.W_OK);
+		await access(imagePath, constants.R_OK | constants.W_OK);
 		return true;
 	} catch (err) {
 		if (err instanceof Error) console.log(`Error: ${err.message}`);
