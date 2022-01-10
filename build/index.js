814e9740122ecf2dd10a1b5cd6313bb4efb00d78
AndreaScacchi <andreascacchi10@gmail.com>
2022-01-10 08:53:44 +0100

    typescript compiled

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// import express and routes
 var express_1 = __importDefault(require("express"));
 var index_1 = __importDefault(require("./routes/index"));
 var app = (0, express_1.default)();
